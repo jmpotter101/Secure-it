2f8a51f1ef9c995b38bdb742c73474fc4a7965b0
John Potter <jmpotter@potterautomotivecompany.com>
2021-01-12 18:18:27 -0600

    Create OverviewScene.java

---
+++ Software/Server_Management/Windows/OverviewScene.java
@@ -0,0 +1,10 @@
+package server_management.overview
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class OverviewScene extends Scene {
+  public OverviewScene(Parent root) {
+    super(root);
+  }
+}
