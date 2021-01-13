c604207f4edc7c00b86f498b5c039e478807cdee
John Potter <jmpotter@potterautomotivecompany.com>
2021-01-12 18:53:11 -0600

    Update OverviewPane.java

--- Software/Server_Management/Windows/OverviewPane.java
+++ Software/Server_Management/Windows/OverviewPane.java
@@ -3,15 +3,30 @@ public server_management.overview;
 import javafx.scene.layout.BorderPane;
 
 public class OverviewPane extends BorderPane {
-  
+  private OverviewLeftPane overviewLeftPane = null;
+  private OverviewTableView overviewTableView = null;
   public OverviewPane() {
-  
     initStuff();
-  
   }
   
-  private initStuff(){
+  private void initStuff(){
+    this.setTop(new Text("In the table we can see the products for sale."));
+    this.setLeft(getOverviewLeftPane());
+    this.setCenter(getOverviewTableView(();
+  }
   
+  private OverviewLeftPane getOverviewLeftPane() {
+    if (overviewLeftPane = null){
+      overviewLeftPane = new OverviewLeftPane();
+    }
+    return overviewLeftPane;
+  }
+  
+  public OverviewTableView getOverviewTableView() {
+    if (overviewTableView == null){
+      overviewTableView = new OverviewTableView();
+    }
+    return overviewTableView; 
   }
   
 }
