1ec1a15bb21c370597477ac5680c49770ec08d87
John Potter <jmpotter@potterautomotivecompany.com>
2021-01-12 18:21:31 -0600

    Create OverviewPane.java

---
+++ Software/Server_Management/Windows/OverviewPane.java
@@ -0,0 +1,17 @@
+public server_management.overview;
+
+import javafx.scene.layout.BorderPane;
+
+public class OverviewPane extends BorderPane {
+  
+  public OverviewPane() {
+  
+    initStuff();
+  
+  }
+  
+  private initStuff(){
+  
+  }
+  
+}
